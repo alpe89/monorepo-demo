9c1337894bf7400aee0bb58d5067c7a092d928b4
alpe89 <a.pertusi89@gmail.com>
2022-02-23 22:24:22 +0100

    initialized design-system

---
+++ libs/design-system/src/lib/design-system.tsx
@@ -0,0 +1,14 @@
+import './design-system.module.css';
+
+/* eslint-disable-next-line */
+export interface DesignSystemProps {}
+
+export function DesignSystem(props: DesignSystemProps) {
+  return (
+    <div>
+      <h1>Welcome to DesignSystem!</h1>
+    </div>
+  );
+}
+
+export default DesignSystem;
